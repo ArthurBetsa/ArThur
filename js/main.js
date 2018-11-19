f2cbfa3ef3f61545ec37a291548e3300f84d2ce7
Артур <Arthur.betsa@gmail.com>
2018-11-19 16:54:57 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+
+function battonClick(formRow) {
+    resultDegree = document.getElementById('resultDegree');
+    row = +formRow.numersRow.value;
+    getdegree = +formRow.degree.value;
+
+    a = row+getdegree;
+    a = Math.pow(row,getdegree);
+    document.write(resultDegree.innerText = a);
+
+}
