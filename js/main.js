7a66e8d7a9ebf8c68882ebd2581448a3f3eab3b2
Артур <Arthur.betsa@gmail.com>
2018-11-21 06:59:34 +0200

    written task in js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
 
-
+/* 1) to count given degree*/
 function battonClick(formRow) {
     resultDegree = document.getElementById('resultDegree');
-    row = +formRow.numersRow.value;
+    row = +formRow.givenData.value;
     getdegree = +formRow.degree.value;
 
     a = row+getdegree;
@@ -11,4 +11,28 @@ function battonClick(formRow) {
     document.write(resultDegree.innerText = a);
 
 }
-//1111
+
+
+/* 2)  found number on Fibom  */
+
+/* 3)  count an array's elements summ */
+/* 4)  sort an array
+       4.1) from smaller to bigger
+       4.2) from bigger to smaller
+ */
+/* 5)  arithmetic mean */
+/* 6)  to count an amount repeat numbers of an array's elements*/
+/* 7)  implement the reverse function */
+/* 8)  to show odd numbers of an array */
+/* 9)  to find en element in an array and implement binar search */
+/* 10) to find maximum divider for all numbers of an array */
+/* 11) to raise array's elements to given degree */
+/* 12) ??? specify */
+/* 13) is an array of palindrome*/
+/* 14) to show even numbers of an array */
+/* 15) remainder operator
+       15.1) array's numbers with remainder
+       16.1 without remainder*/
+/* 17) to put randomly array's elements */
+
+
